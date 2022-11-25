83d884465cb7e9b899b2493ab1e01f70b8b2c691
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-25 15:42:33 -0700

    Create routes for home, new note, show note, and edit note

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
-      Hello
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New note</h1>} />
+      <Route path="/:id" >
+        <Route index element={<h1>Show note</h1>} />
+        <Route path="/edit" element={<h1>Edit note</h1>} />
+      </Route>
+      <Route />
+
+    </Routes>
   )
 }
 
