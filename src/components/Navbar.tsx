4f12f4b62487f812b7198f27e679bdea98619337
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-26 18:03:41 -0700

    Add name to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,9 @@ import { images } from '../constants/index';
 const Navbar = () => {
 
   return (
-    <div className="p-4 bg-[#ff914d] rounded-xl mb-8">
+    <div className="p-4 bg-[#ff914d] rounded-xl mb-8 flex justify-start items-center gap-5">
       <img src={images.logoOutline} alt="logo" className="w-14" />
+      <h1 className="text-[2rem] font-semibold">NoteIt</h1>
     </div>
   )
 }
