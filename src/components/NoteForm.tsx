543d288d979dbb6e7946a06f5af266f8729b1ebc
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-27 16:56:57 -0700

    Add buttons

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -17,12 +17,20 @@ const NoteForm = () => {
           <CreatableReactSelect isMulti className="min-w-[220.5px] block"  />
         </div>
       </div>
-        <div>
-          <label className="block text-white text-xl font-bold mb-2" >
-            Body
-          </label>
-          <textarea className="shadow text-lg appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" rows={12} />
-        </div>
+      <div>
+        <label className="block text-white text-xl font-bold mb-2" >
+          Body
+        </label>
+        <textarea className="shadow mb-4 text-lg appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" rows={12} />
+      </div>
+      <div className="flex gap-4 justify-end">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white text-lg font-bold py-3 px-5 rounded-xl">
+          Save
+        </button>
+        <button className="bg-gray-500 hover:bg-gray-700 text-white text-lg font-bold py-3 px-5 rounded-xl">
+          Cancel
+        </button>
+      </div>
     </form>
   )
 }
