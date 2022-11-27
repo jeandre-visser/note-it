ed0301c42b4e22e01021c901b78cca29d889cd7b
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-27 16:29:31 -0700

    Font color to white

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="p-4 bg-[#ff914d] rounded-xl mb-8 flex justify-start items-center gap-5">
       <img src={images.logoOutline} alt="logo" className="w-14" />
-      <h1 className="text-[2rem] font-semibold">NoteIt</h1>
+      <h1 className="text-[2rem] font-semibold text-white">NoteIt</h1>
     </div>
   )
 }
