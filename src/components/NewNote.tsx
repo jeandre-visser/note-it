e7854d6daa1945830e0f9f3cce620c5193139aec
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-26 17:02:41 -0700

    Start building new note and note form

---
+++ src/components/NewNote.tsx
@@ -0,0 +1,12 @@
+import NoteForm from './NoteForm';
+
+const NewNote = () => {
+  return (
+    <>
+      <div className="mb-5">New Note</div>
+      <NoteForm />
+    </>
+  )
+}
+
+export default NewNote
---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NoteForm = () => {
+  return (
+    <form>
+      <div className="grid gap-3">
+
+      </div>
+    </form>
+  )
+}
+
+export default NoteForm
