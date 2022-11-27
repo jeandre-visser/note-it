a43f6676470b59420ee564752b6078220185d36e
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-27 16:30:03 -0700

    Build form

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const NoteForm = () => {
   return (
-    <form className="w-1/4 bg-[#ff914d] shadow-md rounded p-6 mb-4">
-      <div className="grid gap-3">
-        
+    <form className="w-full max-w-xl bg-[#ff914d] shadow-md rounded-2xl p-6 mb-4" >
+      <div className="gap-12 flex flex-row justify-start">
+        <div>
+          <label className="block text-white text-lg font-bold mb-2" >
+            Title
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text"/>
+        </div>
+        <div>
+          <label className="block text-white text-lg font-bold mb-2" >
+            Tags
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text"/>
+        </div>
       </div>
     </form>
   )
