430329c9570b46d2412cb09049fda939ca393b82
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-27 16:50:28 -0700

    Add body textarea

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import CreatableReactSelect from "react-select/creatable";
 
 const NoteForm = () => {
   return (
-    <form className="w-full max-w-xl bg-[#ff914d] shadow-md rounded-2xl p-6 mb-4" >
-      <div className="gap-12 flex flex-row justify-start">
+    <form className="w-full max-w-xl bg-[#ff914d] shadow-md rounded-2xl p-6" >
+      <div className="gap-12 flex flex-row justify-start mb-4">
         <div>
-          <label className="block text-white text-lg font-bold mb-2" >
+          <label className="block text-white text-xl font-bold mb-2" >
             Title
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text"/>
+          <input className="shadow text-lg appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text"/>
         </div>
         <div>
-          <label className="block text-white text-lg font-bold mb-2" >
+          <label className="block text-white text-xl font-bold mb-2" >
             Tags
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text"/>
+          <CreatableReactSelect isMulti className="min-w-[220.5px] block"  />
         </div>
       </div>
+        <div>
+          <label className="block text-white text-xl font-bold mb-2" >
+            Body
+          </label>
+          <textarea className="shadow text-lg appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" rows={12} />
+        </div>
     </form>
   )
 }
