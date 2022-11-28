3f311972198253b9e496e31fae19cc633b133d84
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-27 18:02:30 -0700

    Adjust input width

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,25 +1,33 @@
 import CreatableReactSelect from "react-select/creatable";
 import { Link } from "react-router-dom";
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
+
+type NoteData = {
+
+}
 
 const NoteForm = () => {
   const titleRef = useRef<HTMLInputElement>(null)
   const markdownRef = useRef<HTMLTextAreaElement>(null)
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  }
+
   return (
-    <form className="w-full max-w-xl bg-[#ff914d] shadow-md rounded-2xl p-6" >
-      <div className="gap-12 flex flex-row justify-start mb-4">
-        <div>
+    <form onSubmit={handleSubmit} className="w-full max-w-xl bg-[#ff914d] shadow-md rounded-2xl p-6" >
+      <div className="gap-4 flex flex-row justify-start mb-4 flex-wrap">
+        <div> 
           <label className="block text-white text-xl font-bold mb-2" >
             Title
           </label>
-          <input className="text-lg rounded w-full py-2 px-3 h-10 text-gray-700 leading-tight focus:outline-offset-2 outline-blue-500" id="title" type="text" ref={titleRef} />
+          <input className="w-full appearance-none text-lg rounded py-2 px-3 text-gray-700 leading-tight focus:outline-offset-2 outline-blue-500" id="title" type="text" ref={titleRef} />
         </div>
-        <div>
+        <div className="w-full sm:w-1/2">
           <label className="block text-white text-xl font-bold mb-2" >
             Tags
           </label>
-          <CreatableReactSelect isMulti className=" text-lg text-gray-700 w-[15rem]"  />
+          <CreatableReactSelect isMulti className=" text-lg text-gray-700"  />
         </div>
       </div>
       <div>
