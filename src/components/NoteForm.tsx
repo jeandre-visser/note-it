9e71eac87f254f443836a9741982904e387e409f
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-27 17:26:58 -0700

    Adjust width

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,7 +1,11 @@
 import CreatableReactSelect from "react-select/creatable";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 const NoteForm = () => {
+  const titleRef = useRef<HTMLInputElement>(null)
+  const markdownRef = useRef<HTMLTextAreaElement>(null)
+
   return (
     <form className="w-full max-w-xl bg-[#ff914d] shadow-md rounded-2xl p-6" >
       <div className="gap-12 flex flex-row justify-start mb-4">
@@ -9,20 +13,20 @@ const NoteForm = () => {
           <label className="block text-white text-xl font-bold mb-2" >
             Title
           </label>
-          <input className="shadow text-lg appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text"/>
+          <input className="text-lg rounded w-full py-2 px-3 h-10 text-gray-700 leading-tight focus:outline-offset-2 outline-blue-500" id="title" type="text" ref={titleRef} />
         </div>
         <div>
           <label className="block text-white text-xl font-bold mb-2" >
             Tags
           </label>
-          <CreatableReactSelect isMulti className="min-w-[220.5px] block"  />
+          <CreatableReactSelect isMulti className=" text-lg text-gray-700 w-[15rem]"  />
         </div>
       </div>
       <div>
         <label className="block text-white text-xl font-bold mb-2" >
           Body
         </label>
-        <textarea className="shadow mb-4 text-lg appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" rows={12} />
+        <textarea className="mb-4 text-lg rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-offset-2 outline-blue-500" id="markdown" rows={12} ref={markdownRef} />
       </div>
       <div className="flex gap-4 justify-end">
         <button className="bg-blue-500 hover:bg-blue-700 text-white text-lg font-bold py-3 px-5 rounded-xl">
